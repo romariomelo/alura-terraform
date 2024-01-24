7aa0862aa6e5a4b77477fa4ee9ec4bd47ac96f4e
Romario Marques Melo <romario.melos@gmail.com>
2024-01-24 09:45:12 -0300

    change in key_name variable: removes the default value and makes it not null

--- vars.tf
+++ vars.tf
@@ -10,10 +10,9 @@ variable "amis" {
 variable "instance_type" {
   type    = string
   default = "t2.micro"
-
 }
 
 variable "key_name" {
-  type    = string
-  default = "key"
+  type     = string
+  nullable = false
 }
